45e38056aebb91d85fe650c0d1ff5d1b62385aef
masu.programmer <masu.programmer@gmail.com>
2023-09-29 09:31:13 +0700

    mebambahkan contoh template

---
+++ template/laravel/docker/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd /var/www
+chmod -R 775 /var/www/storage
+chown -R www-data:www-data /var/www/storage
+composer install
+# php artisan migrate:fresh --seed
+#php artisan cache:clear
+chown  www-data:www-data /var/www/storage/logs/laravel.log
+/usr/bin/supervisord -c /etc/supervisord.conf
