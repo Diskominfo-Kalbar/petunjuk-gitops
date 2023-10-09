0b0c979ca13261821b95e53fd617f83fbc0985dd
masu <masu.programmer@gmail.com>
2023-10-09 23:46:10 +0700

    Penambahan compose untuk local

--- template/laravel/docker/run.sh
+++ template/laravel/docker/run.sh
@@ -6,5 +6,6 @@ chown -R www-data:www-data /var/www/storage
 composer install
 # php artisan migrate:fresh --seed
 #php artisan cache:clear
+php artisan key:generate
 chown  www-data:www-data /var/www/storage/logs/laravel.log
 /usr/bin/supervisord -c /etc/supervisord.conf
